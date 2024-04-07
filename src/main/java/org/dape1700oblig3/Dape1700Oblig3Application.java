cde9b16e8eaafdffd099a14de2dbb0e46a01d802
Marco Josefsen <majos6959@oslomet.no>
2024-04-07 12:25:04 +0200

    Initial commit

---
+++ src/main/java/org/dape1700oblig3/Dape1700Oblig3Application.java
@@ -0,0 +1,13 @@
+package org.dape1700oblig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dape1700Oblig3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dape1700Oblig3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/dape1700oblig3/Dape1700Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dape1700oblig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dape1700Oblig3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
